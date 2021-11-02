c803f24b86a882a4814f0af3774d7ce80f4127aa
kamm8899 <kamm8899@gmail.com>
2021-11-02 15:16:35 -0700

    added resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 function Resume() {
     return (
-        <div>
-            Resume
-        </div>
+        <section>
+            <h1> Jessica Kamman Resume</h1>
+            <hr></hr>
+            <p>
+                View my full Resume: <a href="./assets/PDF/Resume.pdf" alt="resume">Resume</a>
+            </p>
+            <div className="Resume">
+                <p>
+
+                </p>
+
+            </div>
+        </section>
     )
 }
 
