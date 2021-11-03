521b1ce5a9b3d0727b91a9754988a142c99e4cae
kamm8899 <kamm8899@gmail.com>
2021-11-02 19:55:31 -0700

    added data for portifolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/Mountain.jpg";
 
-function Portfolio() {
+
+
+function Portfolio(props) {
+    const currentPortifolio ={
+        name: "projects",
+        description:
+        "Projects on github"
+    };
     return (
         <div>
             <section>
-                <h1>Name</h1>
+                <h1>{capitalizeFirstLetter(currentPortifolio.name)}</h1>
+                <h2>TechStack:</h2>
                 <p>Descriptions</p>
+                <div>
+                    <img
+                        src={photo}
+                        alt="Placeholder Photo"
+                        className="img-thumbnail mx-1"
+                        />
+                </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default Portfolio;
---
+++ src/project.js
@@ -0,0 +1,107 @@
+import Mountain from './assets/images/Mountain.jpg';
+import PasswordGenerator from './assets/images/PasswordGenerator.png';
+import WebAPI from './assets/images/WebAPIQuiz.png';
+import Work_Day from './assets/images/Work_Day_Scheduler.png';
+import WeatherForecast from './assets/images/WeatherForecast.png';
+import NationalPark from './assets/images/NationalPark.png';
+import TeamGenerator from './assets/images/TeamGenerator.png';
+import EmployeeData from './assets/images/EmployeeData.png';
+import MVCDasboard from './assets/images/MVC_Dasboard.png';
+import Mongodb_Social from './assets/images/Mongodb_Social.png';
+import DeepBlue from './assets/images/DeepBlue.png';
+
+export default [
+    {
+        "id": 1,
+        "name": "Movie_Analytics",
+        "image": Mountain,
+        "techStack": "Python",
+        "github": "https://github.com/kamm8899/Movie-Analytics-Country",
+        "deploy": ""
+    }, 
+    {
+        "id": 2,
+        "name": "Password Generator",
+        "image": PasswordGenerator,
+        "techStack": "JavaScript, CSS and HTML",
+        "github": "https://github.com/kamm8899/Password-Generator",
+        "deploy": "https://kamm8899.github.io/Password-Generator/" 
+    },
+    {
+        "id": 3,
+        "name": "Web API Quiz",
+        "image": WebAPI,
+        "techStack": "JavaScript, CSS and HTML",
+        "github": "https://github.com/kamm8899/Web-API-Code-Challenge-Quiz",
+        "deploy": "https://kamm8899.github.io/Web-API-Code-Challenge-Quiz/"
+    },
+    {
+        "id": 4,
+        "name": "Work Day Scheduler",
+        "image": Work_Day,
+        "techStack": "JavaScript, API, CSS and HTML",
+        "github": "https://github.com/kamm8899/Work-Day-Scheduler",
+        "deploy": "https://kamm8899.github.io/Work-Day-Scheduler/"
+    },
+    {
+        "id": 4,
+        "name": "Weather Forecast",
+        "image": WeatherForecast,
+        "techStack": "JavaScript, API, CSS and HTML",
+        "github": "https://github.com/kamm8899/Open-Weather-Weather-Forecast",
+        "deploy": "https://kamm8899.github.io/Open-Weather-Weather-Forecast/"
+    },
+    {
+        "id": 5,
+        "name": "NationalPark",
+        "image": NationalPark,
+        "techStack": "JavaScript, API, CSS and HTML",
+        "github": "https://github.com/kamm8899/National-Park-Project",
+        "deploy": "https://miraj00.github.io/National-Park-Trip-Planner/" 
+    },
+    {
+        "id": 6,
+        "name": "Team Generator",
+        "image": TeamGenerator,
+        "techStack": "JavaScript, Node.Js, CSS and HTML",
+        "github": "https://github.com/kamm8899/Team-Profile-Generator--OOP",
+        "deploy": "" 
+    },
+    {
+        "id": 7,
+        "name": "Employee Database",
+        "image": EmployeeData,
+        "techStack": "JavaScript, Node.Js, Sequelize, Express.Js CSS and HTML",
+        "github": "https://github.com/kamm8899/Team-Profile-Generator--OOP",
+        "deploy": ""  
+    },
+    {
+        "id": 8,
+        "name": "MVC Dashboard",
+        "image": MVCDasboard,
+        "techStack": "JavaScript, Node.Js, Sequelize, Express.Js, SQL, CSS and HTML",
+        "github": "https://github.com/kamm8899/MVC-Dashboard",
+        "deploy": "https://letstechtogether.herokuapp.com/"  
+    },
+    {
+        "id": 9,
+        "name": "Mongodb_Social",
+        "image": Mongodb_Social,
+        "techStack": "JavaScript, Node.Js, Mongoose, MongoDBCSS and HTML",
+        "github": "https://github.com/kamm8899/Mongodb_Social",
+        "deploy": "" 
+    },
+    { 
+    "id": 10,
+    "name": "Deep Blue",
+    "image": DeepBlue,
+    "techStack": "JavaScript, Node.Js, Sequelize,SQL and HTML",
+    "github": "https://github.com/kamm8899/Mongodb_Social",
+    "deploy": "" 
+
+    }
+    
+
+
+]
+
