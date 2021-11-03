60ee5e6c2f51f259f4a6b5efb3685e4d12a7dca2
kamm8899 <kamm8899@gmail.com>
2021-11-02 17:43:01 -0700

    added pdf file

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,21 +1,98 @@
 import React from 'react'
+import { Document, Page} from 'react-pdf';
+import Resume from '../../assets/Resume.pdf'
 
-function Resume() {
+function ResumeF() {
     return (
         <section>
             <h1> Jessica Kamman Resume</h1>
             <hr></hr>
-            <p>
-                View my full Resume: <a href="./assets/PDF/Resume.pdf" alt="resume">Resume</a>
-            </p>
+            <Document file={Resume} options={{workerSrc: "pdf.worker.js"}}>
+                <Page pageNumber={1} />
+            </Document>
+            {/* <a href="http://localhost:3000/homework/React-Portifolio2021/src/assets/Resume.pdf" alt="resume">View My Full Resume</a> */}
             <div className="Resume">
-                <p>
+                <h2>Technical Proficency: Front End </h2>
+                <ul>
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        React.js
+                    </li>
+                    <li>
+                        Bootstrap
+                    </li>
+                    <li>
+                        Materialize
+                    </li>
+                    <li>
+                       JQuery
+                    </li>
+                    <li>
+                        Templating(Handlebars)
+                    </li>
+                </ul>
+                <h2>
+                    Technical Proficency: Back End
+                </h2>
+                <ul>
+                    <li>
+                        CI/CD
+                    </li>
+                    <li>
+                        Containerizaiton
+                    </li>
+                    <li>
+                        Python Programming
+                    </li>
+                    <li>
+                        Java Programming
+                    </li>
+                    <li>
+                        SQL
+                    </li>
+                    <li>
+                        Mongo
+                    </li>
+                    <li>
+                        Node.js
+                    </li>
+                    <li>
+                        Data Structures
+                    </li>
+                    <li>
+                        Express.js
+                    </li>
+                    <li>
+                        Sequelize
+                    </li>
+                    <li>
+                        API(third-Party, RESTful, server-side)
+                    </li>
 
-                </p>
+                </ul>
+
+              <h2>Non Technical Proficiency</h2>  
+              <ul>
+                  <li>
+                      Leadership
+                  </li>
+                  <li>
+                      Agile
+                  </li>
+                  <li>
+                      SDLC
+                  </li>
+              </ul>
+
 
             </div>
         </section>
     )
 }
 
-export default Resume;
+export default ResumeF;
